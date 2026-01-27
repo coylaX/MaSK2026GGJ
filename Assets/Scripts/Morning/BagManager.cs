0db5d003a39b1774d8481c14fe4b0c90aefbb5b2
coylaX <15364272579@163.com>
2026-01-28 04:58:53 +0800

    2

--- Assets/Scripts/Morning/BagManager.cs
+++ Assets/Scripts/Morning/BagManager.cs
@@ -1,18 +1,89 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class BagManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static BagManager Instance;
+
+    private EmotionTraitID emotionTraitID;
+    SaveData data = MorningGameManager.Instance.currentSaveData;
+
+    //UI要素
+    public TextMeshProUGUI pigmentText;     // 颜色数量
+    public TextMeshProUGUI xiText;     // 情绪数量
+    public TextMeshProUGUI nuText;     // 情绪数量
+    public TextMeshProUGUI aiText;     // 情绪数量
+    public TextMeshProUGUI leText;     // 情绪数量
+    #region 颜料、情绪、记忆消耗和更新
+    /// <summary>
+    /// 消耗颜料数并更新UI
+    /// </summary>
+    public void usePigment(int amount)
     {
-        
+        data.morningInventory.pigmentAmount += amount;
+        pigmentText.text = data.morningInventory.pigmentAmount.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 查询颜料数
+    /// </summary>
+    public int getpigment()
     {
-        
+        return data.morningInventory.pigmentAmount;
     }
+
+    ///<summary>
+    ///消耗情绪并更新UI
+    /// </summary>
+    public void useEmotion(EmotionTraitID id, int amount)
+    {
+        switch (id)
+        {
+            case EmotionTraitID.XI: data.morningInventory.xiCount += amount; xiText.text = data.morningInventory.xiCount.ToString(); break;
+            case EmotionTraitID.NU: data.morningInventory.nuCount += amount; nuText.text = data.morningInventory.nuCount.ToString(); break;
+            case EmotionTraitID.AI: data.morningInventory.aiCount += amount; aiText.text = data.morningInventory.aiCount.ToString(); break;
+            case EmotionTraitID.LE: data.morningInventory.leCount += amount; leText.text = data.morningInventory.leCount.ToString(); break;
+        }
+    }
+
+    ///<summary>
+    ///消耗情绪并更新UI
+    /// </summary>
+    public int GetEmotion(EmotionTraitID id)
+    {
+        switch (id)
+        {
+            case EmotionTraitID.XI: return data.morningInventory.xiCount;
+            case EmotionTraitID.NU: return data.morningInventory.nuCount;
+            case EmotionTraitID.AI: return data.morningInventory.aiCount;
+            case EmotionTraitID.LE: return data.morningInventory.leCount;
+            default: return 0;
+        }
+    }
+
+    ///<summary>
+    ///推进记忆进度
+    /// </summary>
+    public void pushMemory()
+    {
+        data.morningInventory.memoryProcess++;
+    }
+
+    ///<summary>
+    ///查询记忆进度
+    /// </summary>
+    public int getMemory()
+    {
+        return data.morningInventory.memoryProcess;
+    }
+    #endregion
+    #region 合成消耗资源（绑定到合成Button上）
+    public void craftUse(EmotionTraitID id)
+    {
+        usePigment(10);
+        useEmotion(id,1);
+    }
+    #endregion
 }
--- Assets/Scripts/Morning/MorningGameDataDefinitions.cs
+++ Assets/Scripts/Morning/MorningGameDataDefinitions.cs
@@ -1,20 +1,11 @@
 using System.Collections.Generic;
 using UnityEngine;
+using static TMPro.Examples.ObjectSpin;
 
 // --- 文件名: MorningGameDataDefinitions.cs ---
 
 #region 1. 枚举定义 (Enums)
-// ==========================================
-// 定义游戏中所有固定的类型选项
-// ==========================================
 
-public enum EmotionType
-{
-    Happy,   // 开心
-    Angry,   // 愤怒
-    Excited, // 兴奋
-    Sad      // 悲伤
-}
 
 #endregion
 
@@ -24,44 +15,7 @@ public enum EmotionType
 // ==========================================
 
 [System.Serializable]
-public class EmotionStorage
-{
-    public int happyCount;
-    public int angryCount;
-    public int excitedCount;
-    public int sadCount;
 
-    public EmotionStorage()
-    {
-        happyCount = 0;
-        angryCount = 0;
-        excitedCount = 0;
-        sadCount = 0;
-    }
-
-    public void AddEmotion(EmotionType type, int amount)
-    {
-        switch (type)
-        {
-            case EmotionType.Happy: happyCount += amount; break;
-            case EmotionType.Angry: angryCount += amount; break;
-            case EmotionType.Excited: excitedCount += amount; break;
-            case EmotionType.Sad: sadCount += amount; break;
-        }
-    }
-
-    public int GetEmotion(EmotionType type)
-    {
-        switch (type)
-        {
-            case EmotionType.Happy: return happyCount;
-            case EmotionType.Angry: return angryCount;
-            case EmotionType.Excited: return excitedCount;
-            case EmotionType.Sad: return sadCount;
-            default: return 0;
-        }
-    }
-}
 #endregion
 
 #region 3. 经营背包数据 (Inventory)
@@ -69,26 +23,32 @@ public class EmotionStorage
 // 包含颜料、情绪、记忆的数据结构
 // ==========================================
 
-[System.Serializable]
+
 public class MorningInventoryData
 {
+    public EmotionTraitID emotionTraitID;
+
     // --- 资源 A: 颜料 ---
     public int pigmentAmount;
 
     // --- 资源 B: 情绪 (引用上面的类) ---
-    public EmotionStorage emotions;
+    public int xiCount;
+    public int nuCount;
+    public int aiCount;
+    public int leCount;
 
-    // --- 资源 C: 记忆 (只存已解锁的ID) ---
-    public List<string> unlockedMemoryIDs;
+    // --- 资源 C: 记忆进度 (只记录进度) ---
+    public int memoryProcess;
 
     public MorningInventoryData()
     {
         pigmentAmount = 0;
-        emotions = new EmotionStorage();
-        unlockedMemoryIDs = new List<string>();
+        xiCount = 0;
+        nuCount = 0;
+        aiCount = 0;
+        leCount = 0;
+        memoryProcess = 0;
     }
-
-    //TODO:
 }
 #endregion
 
